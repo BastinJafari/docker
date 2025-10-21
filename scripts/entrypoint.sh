fe067ac69ced2d208474450408ee50dd81118ee5
David Igón <david@pokecode.net>
2025-10-21 18:19:59 +0200

    add migration execution on app entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -13,4 +13,23 @@ usermod -g "$USER_GID" decidim 2>/dev/null
 chown -R -h "$USER_UID" "$BUNDLE_PATH"
 chgrp -R -h "$USER_GID" "$BUNDLE_PATH"
 
+# Check all the gems are installed or fails.
+bundle check
+if [ $? -ne 0 ]; then
+  echo "❌ Gems in Gemfile are not installed, aborting..."
+  exit 1
+else
+  echo "✅ Gems in Gemfile are installed"
+fi
+
+# Check no migrations are pending migrations
+if [ -z "$SKIP_MIGRATIONS" ]; then
+  bundle exec rails db:migrate
+else
+  echo "⚠️ Skipping migrations"
+fi
+
+echo "✅ Migrations are all up"
+
+echo "🚀 $@"
 /usr/bin/sudo -EH -u decidim "$@"
